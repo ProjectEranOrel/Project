fb61cfdabb3f1413bd563f7ac83b48e4a95c3f72
Eran <Eran@DESKTOP-ILE3GDK>
2018-05-24 22:41:53 +0300

    fdffd

--- ProejctB/src/controllers/TreeScreenController.java
+++ ProejctB/src/controllers/TreeScreenController.java
@@ -1,7 +1,11 @@
 package controllers;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import Entities.Main;
+import Entities.Result;
 import Entities.Taxonomy;
 import Entities.Vars;
 import javafx.beans.property.ReadOnlyStringWrapper;
@@ -22,10 +26,14 @@ public class TreeScreenController
 	public static ObservableList<Taxonomy> selectedData;
 	public TableView<Taxonomy> selectedTable;
 	private ArrayList<Integer> selectedItemsIndexes = new ArrayList<Integer>();
+	private ArrayList<Result> resultList;
+
+	private int startIndex = 0, endIndex;
 
 	@SuppressWarnings("unchecked")
 	public void initialize()
 	{
+		resultList = Result.resultsList;
 		TreeTableColumn<Taxonomy, String> IDCol = new TreeTableColumn<>("Taxonomy ID");
 		IDCol.setCellValueFactory((TreeTableColumn.CellDataFeatures<Taxonomy, String> param) -> 
 		new ReadOnlyStringWrapper(param.getValue().getValue().getTaxID()));
@@ -58,6 +66,7 @@ public class TreeScreenController
 			root.getChildren().add(new TreeItem<Taxonomy>(data.get(i)));
 		treeTable.setRoot(root);
 		treeTable.setShowRoot(false);
+		//markEntries(treeTable.getRoot());
 	}
 
 	public void onAdd()
@@ -74,25 +83,28 @@ public class TreeScreenController
 	{
 		selectedTable.getItems().removeAll(selectedTable.getSelectionModel().getSelectedItems());
 	}
-	public void onClick() {
-	if(selectedItemsIndexes.contains(treeTable.getSelectionModel().getSelectedIndex()))
-		return;
-	
-	selectedItemsIndexes.add(treeTable.getSelectionModel().getSelectedIndex());
+	public void onClick() 
 	{
-		  Thread t1 = new Thread(new Runnable() {
-			  public void run() {
-		TreeItem<Taxonomy> chosen = treeTable.getSelectionModel().getSelectedItem();
+		if(selectedItemsIndexes.contains(treeTable.getSelectionModel().getSelectedIndex()))
+			return;
+		System.out.println("Starting to expand...");
+		selectedItemsIndexes.add(treeTable.getSelectionModel().getSelectedIndex());
+		/*Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				TreeItem<Taxonomy> chosen = treeTable.getSelectionModel().getSelectedItem();
 
+				if(chosen != null && chosen.getValue().isExpandable() && chosen.getChildren().size()==0)//If the chosen table entry has children and they weren't retrieved yet
+					populateTree(chosen,ParseSourceCode.getSons(chosen.getValue()).getSons(),2);
+			}
+		});t1.start();*/
+		TreeItem<Taxonomy> chosen = treeTable.getSelectionModel().getSelectedItem();
 		if(chosen != null && chosen.getValue().isExpandable() && chosen.getChildren().size()==0)//If the chosen table entry has children and they weren't retrieved yet
 			populateTree(chosen,ParseSourceCode.getSons(chosen.getValue()).getSons(),2);
-			  }
-			});t1.start();
-		}
+
 	}
 
-	
-	private void populateTree(TreeItem<Taxonomy> data, ArrayList<Taxonomy> sons,int depth)
+
+	private void populateTree(TreeItem<Taxonomy> chosenTreeItem, ArrayList<Taxonomy> sons,int depth)
 	{
 		for(int i=0;i<sons.size();i++)
 		{
@@ -100,15 +112,115 @@ public class TreeScreenController
 			TreeItem<Taxonomy> sonItem = new TreeItem<Taxonomy>(son);
 			if(son.isExpandable() && depth>0)
 				populateTree(sonItem,son.getSons(),depth-1);
-			data.getChildren().add(sonItem);
+			chosenTreeItem.getChildren().add(sonItem);
+			double numOfEntries = resultList.size()/20;
+			/*if(numOfEntries != (int)numOfEntries)
+				numOfEntries++;*/
+			endIndex = (int)numOfEntries;
+			Thread markingThreads[] = new Thread[20];
+			for(int j=0;j<20;j++)
+			{
+				markingThreads[j] = new Thread(new Runnable()
+				{
+					@Override
+					public void run() 
+					{
+						int localStartIndex = startIndex;
+						int localEndIndex = endIndex;
+						for(int k=localStartIndex;k<localEndIndex;k++)
+						{
+							try 
+							{
+								if(findAncestors(resultList.get(k).getTaxID()).contains(son.getTaxID()))
+								{
+									System.out.println("Found something...");
+									ObservableList<TreeTableColumn<Taxonomy, ?>> columns = treeTable.getColumns();
+									System.out.println("Going thrrough the columns");
+									for(int l=0;l<columns.size();l++)
+									{
+										System.out.println("Column"+l);
+										columns.get(l).setStyle("-fx-background-color:#558C8C");
+									}
+								}
+							} 
+							catch (IOException e) 
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+				});
+				markingThreads[j].start();
+				System.out.println("Thread "+j+" started");
+				startIndex = endIndex;
+				endIndex += endIndex;
+			}
+			for(int j=0;j<20;j++)
+				try {
+					markingThreads[j].join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
+		System.out.println("DONE");
+	}
+
+	/*private void markEntries(TreeItem<Taxonomy> root)
+	{
+
+		ObservableList<TreeItem<Taxonomy>> treeItems = root.getChildren();
+		for(int i=0;i<resultList.size();i++)
+		{
+			try {
+				ArrayList<String> ancestors = findAncestors(resultList.get(i).getTaxID());
+				for(int j=0;j<treeItems.size();j++)
+				{
+
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+	}*/
+
+	public ArrayList<String> findAncestors(String taxID) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new FileReader("nodes.dmp"));
+		String readLine;
+		ArrayList<String> sonColumn = new ArrayList<String>();
+		ArrayList<String> parentColumn = new ArrayList<String>();
+		while((readLine = br.readLine())!=null)
+		{
+			//Add to SonColumn
+			String temp =readLine.substring(0, readLine.indexOf('|'));
+			temp = temp.replaceAll("\\s+", "");
+			sonColumn.add(temp);
+
+			//Add to parentColumn
+			temp = readLine.substring(readLine.indexOf('|')+1,readLine.indexOf('|',readLine.indexOf('|')+1));
+			temp = temp.replaceAll("\\s+", "");
+			parentColumn.add(temp);
+		}
+		ArrayList<String> ancestors = new ArrayList<String>();
+		String currAncestor = parentColumn.get(sonColumn.indexOf(taxID));
+		String nextAncestor = null;
+		ancestors.add(currAncestor);
+		while(true)
+		{
+			nextAncestor = parentColumn.get(sonColumn.indexOf(currAncestor));
+			ancestors.add(nextAncestor);
+			if(currAncestor.equals(nextAncestor)) break;
+			else currAncestor = nextAncestor;
+		}
+		br.close();
+		return ancestors;
 	}
 
 
-	/*public static void setTableData(ArrayList<Taxonomy> tableData)
- {
-  TreeScreenController.tableData = tableData;
- }*/
+
 
 	public void compare()
 	{
