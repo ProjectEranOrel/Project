f4f5587b677650d3a66a37f09ac333efb3d88cc9
Orel <Orel@DESKTOP-KRFJRRE>
2018-06-06 11:55:45 +0300

    dsfsd

--- ProejctB/src/entities/Vars.java
+++ ProejctB/src/entities/Vars.java
@@ -18,6 +18,17 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Vars {
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
 	public static final String genBankLink = "ftp://ftp.ncbi.nlm.nih.gov/genomes/";
 	public static final String searchLink = "https://www.ncbi.nlm.nih.gov/gene/?Term=";
 	public static final String orthoSearchLink = "https://www.ncbi.nlm.nih.gov/gene/";
